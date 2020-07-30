d0cfec142004e13f221944df9139bef810b66547
Logan Barnett <logustus@gmail.com>
2020-07-30 08:43:39 -0700

    add upgrade controller logic
    This also breaks up the extension building logic a bit to be more
    reasonable.

--- miner.js
+++ miner.js
@@ -19,45 +19,88 @@ const createSite = (miner) => {
     STRUCTURE_EXTENSION,
     siteName,
   )
-  console.log(siteResult, sitePos)
-  console.log(JSON.stringify(Game.constructionSites, null, 2))
-  console.log(JSON.stringify(sitePos.look(), null, 2))
-  console.log(STRUCTURE_EXTENSION)
-  miner.memory.site = siteName
-  const site = miner.room.find(FIND_CONSTRUCTION_SITES)
-    .filter(c => c.name == miner.memory.site)[0]
-  return site
+  // console.log('-----createSite-----')
+  // console.log(siteResult, sitePos)
+  // console.log(JSON.stringify(Game.constructionSites, null, 2))
+  // console.log(JSON.stringify(sitePos.look(), null, 2))
+  // console.log(STRUCTURE_EXTENSION)
+  // miner.memory.site = siteName
+  // const site = miner.room.find(FIND_CONSTRUCTION_SITES)
+  //   .filter(c => c.name == miner.memory.site)[0]
+  return [siteResult, siteName]
+}
+
+const getOrCreateSite = (miner) => {
+  if(miner.memory.site == null) {
+    return createSite(miner)
+  } else {
+    return [ 0, miner.memory.site ]
+    // const site = miner.room.find(FIND_CONSTRUCTION_SITES)
+    //     .filter(c => c.name == miner.memory.site)[0]
+    // if(site == null) {
+    //   miner.memory.site = null
+    //   return getOrCreateSite(miner)
+    // } else {
+    //   return [ 0, site.name ]
+    // }
+  }
+}
+
+// Controllers are upgraded by depositing energy into them.
+const taskUpgradeController = (miner) => {
+  if(miner.store.getFreeCapacity('energy') == 0) {
+    miner.memory.taskStack.shift(miner.memory.task)
+    miner.memory.task = 'get-energy'
+  } else {
+    const transferResult = miner.transfer(
+      miner.room.controller,
+      RESOURCE_ENERGY,
+    )
+    if(transferResult == ERR_NOT_IN_RANGE) {
+      miner.moveTo(miner.room.controller)
+    }
+    if(miner.store.getUsedCapacity('energy') == 0) {
+      miner.memory.task = miner.memory.taskStack.pop() || 'build-extension'
+    }
+  }
 }
 
 const taskBuildExtension = (miner) => {
-  if(miner.memory.site == null) {
-    createSite(miner)
-  }
-  // How do we find this one??
-  const site = miner.room.find(FIND_CONSTRUCTION_SITES)
-    .filter(c => c.name == miner.memory.site)[0] || createSite(miner)
-  const buildResult = miner.build(site)
-  switch(buildResult) {
-    // Move to the construction site if we're not near it.
-    case ERR_NOT_IN_RANGE:
-      miner.moveTo(miner.memory.site)
-      break
-    case ERR_NOT_ENOUGH_RESOURCES:
-      // Head to spawner which should have the energy needed.
-      miner.memory.taskStack.shift(miner.memory.task)
-      miner.memory.task = 'get-energy'
-      break
-    case ERR_INVALID_TARGET:
-      if(site.pos.lookFor(FIND_STRUCTURES).length > 0) {
+  const [ siteResult, siteName ] = getOrCreateSite(miner)
+  if(siteResult == ERR_RCL_NOT_ENOUGH) {
+    miner.memory.taskStack.shift(miner.memory.task)
+    miner.memory.task = 'upgrade-controller'
+  } else {
+    console.log('site result', siteResult)
+    const site = miner.room.find(FIND_CONSTRUCTION_SITES)
+      .filter(c => c.name == siteName)[0]
+    // miner.memory.site = site.name
+    console.log('found site', site, miner.memory.site)
+    const buildResult = miner.build(site)
+    switch(buildResult) {
+      case 0:
         miner.memory.site = null
         miner.memory.task = miner.memory.taskStack.pop() || 'harvest'
-      } else if(site.pos.lookFor(FIND_CREEPS).length > 0) {
-          // Creeps block construction. Wait for it to leave.
-      } else {
-        // Something is seriously invalid, try building again.
-        miner.memory.site = null
-      }
-      break
+        break
+      // Move to the construction site if we're not near it.
+      case ERR_NOT_IN_RANGE:
+        miner.moveTo(miner.memory.site)
+        break
+      case ERR_NOT_ENOUGH_RESOURCES:
+        // Head to spawner which should have the energy needed.
+        miner.memory.taskStack.shift(miner.memory.task)
+        miner.memory.task = 'get-energy'
+        break
+      case ERR_INVALID_TARGET:
+        if(site.pos.lookFor(FIND_STRUCTURES).length > 0) {
+        } else if(site.pos.lookFor(FIND_CREEPS).length > 0) {
+            // Creeps block construction. Wait for it to leave.
+        } else {
+          // Something is seriously invalid, try building again.
+          miner.memory.site = null
+        }
+        break
+    }
   }
 }
 
@@ -75,10 +118,9 @@ const taskGetEnergy = (miner) => {
       console.log('Error harvesting from spawner', harvestResult)
   }
   if(miner.store.getFreeCapacity('energy') == 0) {
-    miner.memory.task = miner.memory.taskStack.pop() || 'harvest'
+    miner.memory.task = miner.memory.taskStack.pop() || 'build-extension'
   }
 }
-
 
 const taskHarvest = (spawner, miner) => {
   const source = miner.room.find(FIND_SOURCES)
@@ -152,7 +194,14 @@ module.exports.tickMiner = (miner) => {
       taskGetEnergy(miner)
       break
     case 'harvest':
+      taskHarvest(spawner, miner)
+      break
+    case 'upgrade-controller':
+      taskUpgradeController()
+      break
     default:
+      console.log(`Task ${miner.memory.task} invalid. Falling back to harvesting.`)
+      miner.memory.task = 'harvest'
       taskHarvest(spawner, miner)
   }
 }
